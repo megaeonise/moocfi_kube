6de9af6f4b6a983773cbf4e50648a6b21bf4762d
megaeonise <54655406+megaeonise@users.noreply.github.com>
2025-08-09 13:20:59 +0600

    exercise 1.5 done

--- the_project/index.ts
+++ the_project/index.ts
@@ -3,6 +3,10 @@ const app = express();
 
 const { PORT } = require("./util/config");
 
+app.get("/", (_req: any, res: any) => {
+  res.json({ test: "test" });
+});
+
 const start = () => {
   app.listen(PORT, () => {
     console.log(`Server started in port ${PORT}`);
