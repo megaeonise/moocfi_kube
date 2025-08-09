99ad3fa3c9231938b7bd1c361ae717de79308d2b
megaeonise <54655406+megaeonise@users.noreply.github.com>
2025-08-09 19:48:06 +0600

    exercise 1.6 done

--- log_output/index.ts
+++ log_output/index.ts
@@ -1,6 +1,23 @@
+const express = require("express");
+const app = express();
 const { v4: uuidv4 } = require("uuid");
+
+const { PORT } = require("./util/config");
+const start_date = new Date();
+let log = `${start_date.toISOString()}: ${uuidv4()}`;
 
 setInterval(() => {
   const date = new Date();
-  console.log(`${date.toISOString()}: ${uuidv4()}`);
+  log = `${date.toISOString()}: ${uuidv4()}`;
 }, 5000);
+
+app.get("/", (_req: any, res: any) => {
+  res.send(log);
+});
+
+const start = () => {
+  app.listen(PORT, () => {
+    console.log(`Server started in port ${PORT}`);
+  });
+};
+start();
